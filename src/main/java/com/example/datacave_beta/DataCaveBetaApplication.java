ff3f5f446241389387d34ee2aa24dba06f1d0bfa
«Anton <anton.kotys@gmail.com>
2026-06-01 17:38:48 +0200

    initial concept

--- src/main/java/com/example/datacave_beta/DataCaveBetaApplication.java
+++ src/main/java/com/example/datacave_beta/DataCaveBetaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataCaveBetaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DataCaveBetaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DataCaveBetaApplication.class, args);
+	}
 
 }
